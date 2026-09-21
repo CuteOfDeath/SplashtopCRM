a991ad4a25671e7dd656cd4db777df39e6d9b6bf
CuteOfDeath <sebrych8@gmail.com>
2026-09-21 10:11:10 +0200

    guh

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -627,7 +627,7 @@ export default function Dashboard() {
         <>
             <Navbar expand="lg" className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark" style={{borderRadius: "10px", margin: "20px"}}>
                 <Container fluid>
-                    <Navbar.Brand as={Link} to={`${import.meta.env.BASE_URL}/`}>Cemit CRM</Navbar.Brand>
+                    <Navbar.Brand as={Link} to={`${import.meta.env.BASE_URL}/`}>CRM</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
